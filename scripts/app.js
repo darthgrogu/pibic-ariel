059a020fab0b9f0829c030350f1de4d278ffb1b3
Luiz Melo PC <melostrader@gmail.com>
2024-11-11 06:43:25 -0400

    testing...

--- scripts/app.js
+++ scripts/app.js
@@ -25,7 +25,9 @@ function initEventListeners() {
 function fillPageData() {
   const urlParams = new URLSearchParams(window.location.search);
   const id = parseInt(urlParams.get("id"));
+  const fragment = document.createDocumentFragment();
 
+  let slides = document.querySelector(".slides");
   let vernacular = document.querySelector(".vernacular");
   let nomecientifico = document.querySelector(".nome-cientifico");
   let autor = document.querySelector(".autor");
@@ -39,6 +41,15 @@ function fillPageData() {
   const result = dados.find((item) => item.id === id);
   console.log(result);
 
+  result.imagelist.forEach((image) => {
+    const img = document.createElement("img");
+    img.setAttribute("src", image);
+    img.classList.add("slide");
+    fragment.appendChild(img);
+  });
+
+  slides.appendChild(fragment);
+
   vernacular.textContent = result.vernacular;
   nomecientifico.textContent = result.nomecientifico;
   autor.textContent = result.autor;
@@ -50,5 +61,13 @@ function fillPageData() {
   descricao.textContent = result.descricao;
   distribuicao.textContent = result.distribuicao;
 
-  let conteudo = document.querySelector(".conteudo");
+  const mapa_container = document.querySelector(".mapa-container");
+  const fragment2 = document.createDocumentFragment();
+  const img = document.createElement("img");
+  console.log(result.distribuicao_mapa);
+  img.setAttribute("src", result.distribuicao_mapa);
+  img.classList.add("mapa-distribuicao");
+  fragment2.appendChild(img);
+
+  mapa_container.appendChild(fragment2);
 }
